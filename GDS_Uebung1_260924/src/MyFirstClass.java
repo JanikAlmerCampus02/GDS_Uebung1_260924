20531664da42a7e74ff2b6c8b1139fe466fff11d
janikalmercode <janikalmerbusiness@gmail.com>
2024-09-26 15:23:57 +0200

    first class

---
+++ GDS_Uebung1_260924/src/MyFirstClass.java
@@ -0,0 +1,9 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("Zeile zwei");
+
+        System.out.println("Programm Ende");
+    }
+}
---
+++ GDS_Uebung1_260924/src/VariablesDemo.java
@@ -0,0 +1,9 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int i = 100;
+        System.out.println(i);
+
+        //soutv
+        System.out.println("i = " + i);
+    }
+}
